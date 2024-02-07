ae1cfcbeecaf2d50e3d9ef8f5d4cbc5abd3f51e4
Lucas P <lucas.castropereira@livecampus.tech>
2024-02-07 22:44:54 +0100

    Amelioration details et list

--- mon_projet/views.py
+++ mon_projet/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Avg
 from collections import defaultdict
+from .models import Etudiant
 from .models import Formulaire
 from .models import ReponsesFormulaire
 
@@ -14,17 +15,17 @@ def detail_forms_admin(request, id_formulaire):
     moy_progression = reponses.aggregate(avg_progression=Avg('progression'))['avg_progression'] or 0
     count_difficulte = defaultdict(int)
     count_maitrise = defaultdict(int)
+
+    print(reponses.values())
     
     for reponse in reponses:
         if reponse.difficulte:
             count_difficulte[reponse.difficulte] += 1
         if reponse.maitrise:
             count_maitrise[reponse.maitrise] += 1
-    
-    for key,value in count_difficulte.items():
-        print(f'{key} : {value}')
 
     return render(request, 'formulaires_details.html', {
+        'id_formulaire' : id_formulaire,
         'reponses': reponses,
         'total_reponses': total_reponses,
         'moy_progression': moy_progression,
