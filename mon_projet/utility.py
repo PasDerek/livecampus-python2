babf684b29e39440a5ea180f406fdd30105492ce
Lucas P <lucas.castropereira@livecampus.tech>
2024-02-10 14:22:15 +0100

    utility - jwt

--- mon_projet/utility.py
+++ mon_projet/utility.py
@@ -1,8 +1,19 @@
 import bcrypt
 from . import env
+import datetime
+import jwt
 
 def hash_password(password):
     return bcrypt.hashpw(str.encode(password), env.secrets['salt'])
 
 def check_hash_password(password, db_password):
     return bcrypt.checkpw(str.encode(password), db_password)
+
+def generate_jwt(username):
+    #Generation de token
+    payload = {
+        'username': username,
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+    }
+    token = jwt.encode(payload, env.secrets['Jwt'], algorithm='HS256')
+    return token
