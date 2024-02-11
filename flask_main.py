e0dece33764979e8610504c9a8d6686793906fd9
Lucas P <lucas.castropereira@livecampus.tech>
2024-02-11 16:23:04 +0100

    avancée flask

--- flask_main.py
+++ flask_main.py
@@ -1,23 +1,26 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 import mysql.connector
+import sys
+sys.path.append('mon_projet')
+from mon_projet import env
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+app.secret_key = env.SECRETS["FLASK"]
 
-@app.route('/<string:url>', methods=['POST'])
-def update_responses(url):
+@app.route('/', methods=['POST'])
+@cross_origin()
+def update_responses():
     data = request.json
+    print(data)
 
-    id_formulaire = data.get('id_formulaire')
-    username = data.get('username')
-    progression = data.get('progression')
-    difficulte = data.get('difficulte')
-    maitrise = data.get('maitrise')
-
-    print(id_formulaire)
-    print(username)
-    print(progression)
-    print(difficulte)
-    print(maitrise)
+    # id_formulaire = data.get('id_formulaire')
+    # username = data.get('username')
+    # progression = data.get('progression')
+    # difficulte = data.get('difficulte')
+    # maitrise = data.get('maitrise')
 
     return jsonify({'message': 'bla bla bla'})
 
